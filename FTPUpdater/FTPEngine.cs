093f0b7b96816ce90201e09d9541a7bba5884cb9
wendall@hersheysmm.com <wendall@hersheysmm.com>
2018-07-03 19:01:12 -0400

    first commit

---
+++ FTPUpdater/FTPEngine.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace FTPUpdater
+{
+    public class FTPEngine
+    {
+        public static string FtpUrl = "ftp://208.113.130.91/files/Wendall/TimeApp/";
+        public static NetworkCredential FtpCredentials = new NetworkCredential("abcodeblocks", "Coding4HisGlory!");
+
+        /// <summary>
+        /// Returns list of Versions available on ftp update server.
+        /// </summary>
+        /// <returns></returns>
+        public static List<Version> GetUpdateVersions()
+        {
+            try
+            {
+                List<Version> directorys = new List<Version>();
+                FtpWebRequest ftpWebRequest = (FtpWebRequest)WebRequest.Create(FtpUrl);
+                ftpWebRequest.Credentials = FtpCredentials;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                FtpWebResponse response = (FtpWebResponse)ftpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(response.GetResponseStream() ?? throw new Exception("Respose is null"));
+                string line = streamReader.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    directorys.Add(new Version(line));
+                    line = streamReader.ReadLine();
+                }
+
+                streamReader.Close();
+                return directorys;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void DownloadFtpDirectory(string url, string localPath)
+        {
+            FtpWebRequest listRequest = (FtpWebRequest)WebRequest.Create(url);
+            listRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            listRequest.Credentials = FtpCredentials;
+            List<string> lines = new List<string>();
+            using (FtpWebResponse listResponse = (FtpWebResponse)listRequest.GetResponse())
+            using (Stream listStream = listResponse.GetResponseStream())
+            using (StreamReader listReader = new StreamReader(listStream ?? throw new Exception("Value cannnot be null!")))
+            {
+                while (!listReader.EndOfStream)
+                {
+                    lines.Add(listReader.ReadLine());
+                }
+            }
+
+            foreach (string line in lines)
+            {
+                string[] tokens = line.Split(new[] { ' ' }, 9, StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[8];
+                string permissions = tokens[0];
+                string localFilePath = Path.Combine(localPath, name);
+                string fileUrl = url + name;
+                if (permissions[0] == 'd')// this is a directory
+                {
+                    if (!Directory.Exists(localFilePath))
+                    {
+                        Directory.CreateDirectory(localFilePath);
+                    }
+
+                    foreach (string filename in Directory.GetFiles(localFilePath))
+                    {
+                        File.Delete(filename);
+                    }
+
+                    DownloadFtpDirectory(fileUrl + "/", localFilePath);
+                }
+                else// this is a file
+                {
+                    FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(fileUrl);
+                    downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                    downloadRequest.Credentials = FtpCredentials;
+                    try
+                    {
+
+                        if(File.Exists(localFilePath))
+                            File.Delete(localFilePath);
+                        using (FtpWebResponse downloadResponse = (FtpWebResponse)downloadRequest.GetResponse())
+                        using (Stream sourceStream = downloadResponse.GetResponseStream())
+                        using (Stream targetStream = File.Create(localFilePath))
+                        {
+                            byte[] buffer = new byte[10240];
+                            int read;
+                            while (sourceStream != null && (read = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                targetStream.Write(buffer, 0, read);
+                            }
+                        }
+                    }
+                    catch (WebException e)
+                    {
+                        MessageBox.Show(fileUrl + " : " + ((FtpWebResponse)e.Response).StatusDescription);
+                    }
+                }
+            }
+        }
+
+        public static void Update(Version latest)
+        {
+            if (Program.CurrentDirectory == "" || !Directory.Exists(Program.CurrentDirectory))
+            {
+                MessageBox.Show("Invalid directory!");
+                return;
+            }
+            try
+            {
+                DownloadFtpDirectory(FtpUrl + latest + "/", Program.CurrentDirectory);
+                MessageBox.Show("Update successful!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Update failed!");
+            }
+        }
+    }
+}
---
+++ FTPUpdater/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace FTPUpdater
+{
+    static class Program
+    {
+        public static Version CurrentVersion;
+        
+        /// <summary>
+        /// Directory where the update is downloaded to
+        /// and the old files and folders are deleted.
+        /// </summary>
+        public static string CurrentDirectory;
+
+        public static string ParentProcess = "";
+        
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                MessageBox.Show("Argument missmatch!! \nArgments are: string current version, string current directory.");
+                return;
+            }
+
+            if (args.Length >= 3)
+            {
+                ParentProcess = args[2];
+            }
+            
+            CurrentVersion = new Version(args[0]);
+            CurrentDirectory = args[1];
+
+            try
+            {
+                UpdateWindow window = new UpdateWindow();
+                window.ShowDialog();
+            }
+            catch (Exception )// Other wise if the server is wrong or somethingwe get a hidious error message
+            {
+                // do nothing
+            }
+        }
+        
+        public static void StopParentProcess()
+        {
+            if(string.IsNullOrEmpty(Program.ParentProcess))
+                return;
+            
+            foreach (Process process in Process.GetProcessesByName(Program.ParentProcess))
+            {
+                process.Kill();
+            }
+        }
+
+        public static void StartParentProcess()
+        {
+            Process.Start(Path.Combine(Program.CurrentDirectory, Program.ParentProcess));
+        }
+    }
+}
